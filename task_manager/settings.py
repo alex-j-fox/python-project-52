cd79418264f728c1c577fa805c5115cb541cfd28
alex-j-fox <alex-j-fox@yandex.ru>
2024-08-29 03:07:41 +0300

    CI: add ALLOWED_HOSTS in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,6 +31,10 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['webserver', 'localhost', '127.0.0.1']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
