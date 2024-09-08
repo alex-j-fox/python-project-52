e5daab6b8e1dfa81d4a5fe32dfe66f159497776f
alex-j-fox <alex-j-fox@yandex.ru>
2024-09-09 01:40:03 +0300

    CI:  django_ci.yml, setup.cfg, settings.py
    task_manager/settings.py :
    	- добавил приложения в INSTALLED_APPS,
    	- добавил опцию 'min_length' в валидаторы, отключил проверку пароля в списке распространенных паролей и является ли он полностью числовым
    	- отключил USE_L10N
    setup.cfg:
    	 - добавил настройки для pytest
    .github/workflows/django_ci.yml:
    	 - обновил название шага

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'task_manager',
+    'task_manager.users',
+    'task_manager.labels',
 ]
 
 MIDDLEWARE = [
@@ -87,14 +89,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 3,
+        },
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
+
+# LOGIN_REDIRECT_URL = 'index'
+# LOGIN_URL = 'login'
+# LOGOUT_REDIRECT_URL = 'login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -113,8 +122,6 @@ LOCALE_PATHS = (
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
