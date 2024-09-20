2d254f8629fb9215bebe96d874cdba2b624653a5
alex-j-fox <alex-j-fox@yandex.ru>
2024-09-20 10:37:08 +0300

    Chore: users/admin.py, context_processors.py task_manager/context_processors.py - добавил документацию task_manager/users/admin.py - добавил плюшек для админ-панели

--- task_manager/context_processors.py
+++ task_manager/context_processors.py
@@ -2,6 +2,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 def navbar(request):
+    """
+    Provides the navbar items for the navigation bar.
+
+    It checks if the user is authenticated and returns the corresponding
+    navbar items. If the user is authenticated, it returns the links to the
+    users, statuses, labels, and tasks pages. If the user is not authenticated,
+    it returns the links to the login and registration pages.
+
+    :param request: The HTTP request.
+    :type request: HttpRequest
+    :return: A dictionary containing the navbar items.
+    :rtype: dict
+    """
     user = request.user
     navbar_items = [{
         'label': _('Users'),
@@ -10,6 +23,7 @@ def navbar(request):
         'align': ''
     }]
     if request.user.is_authenticated:
+        # Add the links to the users, statuses, labels, and tasks pages
         navbar_items.append({
             'label': _('Statuses'),
             'url': '/statuses/',
@@ -28,11 +42,13 @@ def navbar(request):
             'class': 'nav-link',
             'align': ''
         })
+        # Add the link to the user's profile
         navbar_items.append({
             'label': _('Wellcome') + ', ' + user.username,
             'class': 'nav-link',
             'align': 'ms-auto'
         })
+        # Add the logout button
         navbar_items.append({
             'label': _('Logout'),
             'url': '/logout/',
@@ -41,6 +57,7 @@ def navbar(request):
             'align': ''
         })
     else:
+        # Add the links to the login and registration pages
         navbar_items.append({
             'label': _('Login'),
             'url': '/login/',
