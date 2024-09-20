c1bf32032e83762dfa22f098e25147d240d86f5f
alex-j-fox <alex-j-fox@yandex.ru>
2024-09-20 10:37:08 +0300

    Chore: users/admin.py, context_processors.py task_manager/context_processors.py - добавил документацию task_manager/users/admin.py - добавил плюшек для админ-панели

--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+admin.site.unregister(User)
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'first_name', 'last_name', 'created_at')
-    list_filter = ('username', 'first_name', 'last_name', 'created_at')
+    list_display = (
+    'id', 'username', 'first_name', 'last_name', 'date_joined', 'is_staff', 'is_active')
+    list_display_links = ('id', 'username')
+    list_filter = ('username', 'first_name', 'last_name', 'date_joined')
     search_fields = ('username', 'first_name', 'last_name')
     prepopulated_fields = {'username': ('username',)}
-    date_hierarchy = 'created_at'
-    ordering = ('-created_at',)
+    date_hierarchy = 'date_joined'
+    ordering = ('-date_joined',)
