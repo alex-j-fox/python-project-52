e62e658b894619ec8b5d3eb1cc1cbdca24827d93
alex-j-fox <alex-j-fox@yandex.ru>
2024-09-23 00:55:26 +0300

    Refactoring: users/view.py - вынес метод get_context_data в класс ContextMixin

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views import View
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, FormView
 
 from task_manager.users.forms import UserForm
 from task_manager.users.mixins import AuthAndProfileOwnershipMixin
@@ -23,38 +23,38 @@ class IndexView(View):
                       context={'users': users})
 
 
-class UserCreateView(SuccessMessageMixin, CreateView):
+class ContextMixin(SuccessMessageMixin, FormView):
+    title = ''
+    action = ''
+
+    def get_context_data(self, **kwargs):
+        """
+        Передача данных (название формы, текст кнопки) в контекст
+        """
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
+        context['action'] = self.action
+        return context
+
+
+class UserCreateView(ContextMixin, CreateView):
     template_name = 'users/create.html'
     form_class = UserForm
     success_url = reverse_lazy('users_index')
     success_message = _('User successfully registered')
     model = get_user_model()
-
-    def get_context_data(self, **kwargs):
-        """
-        Передача данных (название формы, текст кнопки) в контекст
-        """
-        context = super().get_context_data(**kwargs)
-        context['title'] = _('Registration')
-        context['action'] = _('Register')
-        return context
+    title = _('Registration')
+    action = _('Register')
 
 
-class UserUpdateView(SuccessMessageMixin, AuthAndProfileOwnershipMixin, UpdateView):
+class UserUpdateView(AuthAndProfileOwnershipMixin, ContextMixin, UpdateView):
     model = get_user_model()
     template_name = 'users/update.html'
     form_class = UserForm
     success_url = reverse_lazy('users_index')
     success_message = _('User successfully updated')
-
-    def get_context_data(self, **kwargs):
-        """
-        Передача данных (название формы, текст кнопки) в контекст
-        """
-        context = super().get_context_data(**kwargs)
-        context['title'] = _('Change user')
-        context['action'] = _('Change')
-        return context
+    title = _('Change user')
+    action = _('Change')
 
 
 class UserDeleteView(SuccessMessageMixin, AuthAndProfileOwnershipMixin, DeleteView):
