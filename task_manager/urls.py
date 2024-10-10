6b15118058a8a103a285b51c9140bac1a43395ce
alex-j-fox <alex-j-fox@yandex.ru>
2024-10-10 23:53:52 +0300

    Feat: task_manager/urls.py - добавил путь для Tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
 ]
