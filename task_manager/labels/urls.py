3ea4a75180d137f32abb577f81c8c1a8faee8596
alex-j-fox <alex-j-fox@yandex.ru>
2024-10-13 16:09:57 +0300

    Fix: добавил / к path в urls.py статусов и меток

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -5,6 +5,6 @@ from task_manager.labels import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='labels_index'),
     path('create/', views.LabelCreateView.as_view(), name='labels_create'),
-    path('<int:pk>/update', views.LabelUpdateView.as_view(), name='labels_update'),
-    path('<int:pk>/delete', views.LabelDeleteView.as_view(), name='labels_delete'),
+    path('<int:pk>/update/', views.LabelUpdateView.as_view(), name='labels_update'),
+    path('<int:pk>/delete/', views.LabelDeleteView.as_view(), name='labels_delete'),
 ]
